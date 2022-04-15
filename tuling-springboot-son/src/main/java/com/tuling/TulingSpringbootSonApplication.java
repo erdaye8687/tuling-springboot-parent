ee80459451bd12a2e9b09b0d94f1e70f02c75b91
MrYuan <2197944956@qq.com>
2022-04-15 23:09:10 +0800

    *: test

---
+++ tuling-springboot-son/src/main/java/com/tuling/TulingSpringbootSonApplication.java
@@ -0,0 +1,13 @@
+package com.tuling;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TulingSpringbootSonApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TulingSpringbootSonApplication.class, args);
+    }
+
+}
